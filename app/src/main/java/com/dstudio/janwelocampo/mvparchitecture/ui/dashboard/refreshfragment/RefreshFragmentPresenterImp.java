dbca7ddd5347eb24a93a983dc5ec2bf42ef2cdcd
janwel ocampo <janwel.ocampo@gmail.com>
2018-01-20 18:27:02 +0800

    Added firebase to production build

--- app/src/main/java/com/dstudio/janwelocampo/mvparchitecture/ui/dashboard/refreshfragment/RefreshFragmentPresenterImp.java
+++ app/src/main/java/com/dstudio/janwelocampo/mvparchitecture/ui/dashboard/refreshfragment/RefreshFragmentPresenterImp.java
@@ -1,12 +1,14 @@
 package com.dstudio.janwelocampo.mvparchitecture.ui.dashboard.refreshfragment;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.os.Environment;
 
 import com.dstudio.janwelocampo.mvparchitecture.MvpApp;
 import com.dstudio.janwelocampo.mvparchitecture.R;
 import com.dstudio.janwelocampo.mvparchitecture.data.database.DatabaseHelper;
 import com.dstudio.janwelocampo.mvparchitecture.ui.base.MvpView;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -21,10 +23,12 @@ public class RefreshFragmentPresenterImp implements RefreshFragmentPresenter {
 
     private RefreshFragmentView refreshFragmentView;
     private Context context;
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     public RefreshFragmentPresenterImp(Context context) {
         ((MvpApp) context).getAppComponent(context).inject(this);
         this.context = context;
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(context);
     }
 
     @Override
@@ -44,6 +48,10 @@ public class RefreshFragmentPresenterImp implements RefreshFragmentPresenter {
 
     @Override
     public void onClickCopy() {
+        Bundle params = new Bundle();
+        params.putString("userClick", "Copy db");
+        mFirebaseAnalytics.logEvent("onClickCopy", params);
+
         try {
             File sd = Environment.getExternalStorageDirectory();
             File currentDB = context.getDatabasePath(DatabaseHelper.DATABASE_NAME);
